a18abf40e6155f56f33673455fbc389f7287fac2
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2019-11-07 15:08:27 -0800

    fix env

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const port = 3000;
+require('dotenv').config();
+
+// const port = 3000;
 const Config = require('./config/config');
 const database = require('./config/database.config');
 const express = require('express');
 
-// require('dotenv').config();
+require('dotenv').config();
 
 
 let app = express();
@@ -21,6 +23,9 @@ require('./config/passport')();
 // app.listen(PORT, (req, res) => {
 //     console.log(`Server listen on port ${PORT}...`);
 // });
+app.listen(process.env.PORT, () => {
+    console.log(`Server listen on port ${process.env.PORT}...`)
+})
 
-app.listen(port);
-console.log(`Server listen on port ${port}...`);
+// app.listen(port);
+// console.log(`Server listen on port ${port}...`);
