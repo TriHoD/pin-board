689e41f5e539aa80b4ed70093a68062f85644505
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2019-11-07 14:26:20 -0800

    try

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
     development: {
-        // connectionString: 'mongodb://localhost/pinBoard',
-        connectionString: process.env.MONGODB_URI,
+        connectionString: 'mongodb://localhost/pinBoard',
+        // connectionString: process.env.MONGODB_URI,
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     production:{
