3777ea6043b66ad583430e9a553bbf7590d54a42
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2019-11-07 14:01:39 -0800

    heroku deployment set up

--- config/config.js
+++ config/config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 module.exports = {
     development: {
-        connectionString: 'mongodb://localhost/pinBoard',
+        // connectionString: 'mongodb://localhost/pinBoard',
+        connectionString: process.env.MONGODB_URI,
         rootPath: path.normalize(path.join(__dirname, '../'))
     },
     production:{
